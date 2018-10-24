7ba95cb0761d93cc9343abc24758cee1be4cf8ed
Zacharias Tyllström <zache21@hotmail.se>
2018-10-24 11:14:56 +0200

    Begun to create tests for TDD and got the debugger running.

--- compileZac.bat
+++ compileZac.bat
@@ -2,7 +2,7 @@
 set JDK_HOME=C:\PROGRA~1\Java\jdk1.8.0_71
 @echo "Compiling code ..."
 @mkdir bin
-%JDK_HOME%/bin/javac.exe -d bin -cp src src/ltu/Main.java src/ltu/CalendarImpl.java
+%JDK_HOME%/bin/javac.exe -d bin -cp src src/ltu/Apples2Apples.java
 @echo "Compiling tests ..."
 set JUNIT=E:\D7032E\assignment3testing\assignment3testing\lib\org.junit4-4.3.1.jar
-%JDK_HOME%/bin/javac.exe -d bin -cp %JUNIT%;src src/ltu/Apples2Apples.java
+%JDK_HOME%/bin/javac.exe -d bin -cp %JUNIT%;src src/ltu/Apples2ApplesTest.java
